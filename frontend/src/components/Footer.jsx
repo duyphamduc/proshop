e438deb7c2986054fe48381754aa31cc9ce03329
Duy Pham <duypham93@gmail.com>
2024-01-18 00:01:53 -0600

    Custom theme and logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-  const year = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">&copy; {year} ProShop</Col>
+          <Col className="text-center py-3">
+            <p>Proshop &copy; {currentYear}</p>
+          </Col>
         </Row>
       </Container>
     </footer>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ProShop" width="50" height="50" />
+            ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+            <Nav className="ms-auto">
               <Nav.Link href="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
