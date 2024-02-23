e73d36203727e66065f07e4e5b4fd29fef75588f
Duy Pham <duypham93@gmail.com>
2024-02-23 07:05:24 -0600

    User Verification & jwt auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -9,6 +10,13 @@ const port = process.env.PORT || 4000;
 
 connectDB();
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
